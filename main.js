3a491f9eb4938d1d249ebdccb46d724adae4bc61
lakshya-Whitehatjr <83779709+lakshya-Whitehatjr@users.noreply.github.com>
2021-07-16 19:50:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("Hi");
+var first_name = "Lakshya";
+console.log(first_name);
+var last_name = " Agiwal";
+console.log(last_name);
+var full_name = first_name.concat( last_name);
+console.log(full_name);
+var length_of_name = full_name.length;
+console.log(length_of_name);
+var num = 9;
+console.log(num);
+ console.log( Math.sqrt(num));
+ var num_1 = 6;
+ console.log(num + num_1);
+
